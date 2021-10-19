a6ab90edee5f96d5da4962fa8aaa5f1c22194a7c
Hao Nguyen <doibanchan1997@gmail.com>
2021-10-19 23:00:32 +0700

    add feature create todo list

--- lib/widgetPage/todo_list_page.dart
+++ lib/widgetPage/todo_list_page.dart
@@ -18,6 +18,14 @@ class _TodoListPage extends State<TodoListPage> {
     {'content': "di ngu", 'isDone': true},
   ];
 
+  bool flagAddTodo = false;
+
+  void _addTodoList() {
+    setState(() {
+      flagAddTodo = !flagAddTodo;
+    });
+  }
+
   void _updateState(index) {
     var target = todo
         .firstWhere((element) => element['content'] == todo[index]['content']);
@@ -27,32 +35,95 @@ class _TodoListPage extends State<TodoListPage> {
     setState(() {});
   }
 
+  Widget _listTodo() {
+    return ListView.builder(
+        itemCount: todo.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: Card(
+              color: todo[index]['isDone'] ? Colors.white24 : Colors.white,
+              child: ListTile(
+                onTap: () => {_updateState(index)},
+                leading: Checkbox(
+                  value: todo[index]['isDone'],
+                  onChanged: (e) => {},
+                  shape: const CircleBorder(),
+                ),
+                title: Text(
+                  todo[index]['content'],
+                  style: TextStyle(
+                      decoration: todo[index]['isDone']
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _createTodo() {
+    return Text('Create Todo');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            color: Colors.white60,
-          ),
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          color: Colors.white60,
         ),
-        body: ListView.builder(
-            itemCount: todo.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8),
-                child: Card(
-                  child: ListTile(
-                    onTap: () => {_updateState(index)},
-                    leading: Checkbox(
-                      value: todo[index]['isDone'],
-                      onChanged: (e) => {},
-                    ),
-                    title: Text(todo[index]['content']),
-                  ),
-                ),
-              );
-            }));
+      ),
+      body: Container(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              child: _listTodo(),
+            ),
+            Visibility(
+                visible: flagAddTodo,
+                child: GestureDetector(
+                  onTap: () => {_addTodoList()},
+                  child: Positioned(
+                      top: 0,
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.black45,
+                        ),
+                      )),
+                )),
+            Visibility(
+                visible: flagAddTodo,
+                child: Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50),
+                            topRight: Radius.circular(50)),
+                        color: Colors.white,
+                      ),
+                      height: 500,
+                      padding: const EdgeInsets.all(5.0),
+                      alignment: Alignment.bottomCenter,
+                      child: _createTodo(),
+                    )))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {_addTodoList()},
+      ),
+    );
   }
 }
